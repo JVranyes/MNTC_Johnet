62124a49457db0b4ff8699bd1e8d31653ab6c6c7
JVranyes <125674915+JVranyes@users.noreply.github.com>
2024-02-26 12:54:44 -0600

    Initial commit

---
+++ Tables/dbo.ApptVerifcationHistory.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[ApptVerifcationHistory] (
+  [diffgr:id] [varchar](512) NULL,
+  [msdata:rowOrder] [varchar](512) NULL,
+  [xmlns:diffgr] [varchar](512) NULL,
+  [xmlns:msdata] [varchar](512) NULL,
+  [ApptVerificationHistoryId] [varchar](512) NULL,
+  [PlanId] [varchar](512) NULL,
+  [Status] [varchar](512) NULL,
+  [UpdatedDate] [varchar](512) NULL,
+  [Description] [varchar](512) NULL,
+  [ShowForTelevox] [varchar](512) NULL
+)
+ON [PRIMARY]
+GO
---
+++ Tables/dbo.Credible_Database_Schema_Columns_DataTypesSizes.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Credible_Database_Schema_Columns_DataTypesSizes] (
+  [TABLE_NAME] [varchar](100) NULL,
+  [COLUMN_NAME] [varchar](100) NULL,
+  [IS_NULLABLE] [varchar](50) NULL,
+  [DATA_TYPE] [varchar](50) NULL,
+  [CHARACTER_MAXIMUM_LENGTH] [varchar](50) NULL,
+  [TABLE_CATALOG] [varchar](50) NULL,
+  [TABLE_SCHEMA] [varchar](50) NULL,
+  [ORDINAL_POSITION] [varchar](50) NULL,
+  [DATETIME_PRECISION] [varchar](50) NULL,
+  [Column1] [varchar](50) NULL,
+  [Column2] [int] NULL
+)
+ON [PRIMARY]
+GO
